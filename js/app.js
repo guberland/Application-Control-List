60952b43f363bcdc38d8291f29947ebc43a6a9f6
guberland <chaojiang1189@gmail.com>
2017-11-23 22:41:29 -0800

    scrollable list added to show names

--- js/app.js
+++ js/app.js
@@ -4,32 +4,36 @@
 var my_data;
 
 var getData = function (){
-    var theURL = "http://jobs.fortinet.com/test.json"
+    var theURL = "/test.json"
 
      $.get(theURL, function( my_var ) {
     new_var = my_var.substring(0,my_var.length-1);
     my_data = JSON.parse( new_var);
-    show_results(my_var);
+    console.log(my_data);
+    // normalizeJSON(my_data);
+    $.each(my_data,function(i,app){
+        $('#list').append('<h3>'+my_data[i][0]+'</h3>');
+    })
+
 }, 'text');     //data from to another place in the load
 };
 
 window.onload = getData;
 
-
-function show_results(results) {
- alert(my_data);
-}
+// var normalizeJSON = function(data){
+//     for each
+// }
 
 
 console.log(my_data);
 
 
-var ViewModel = function() {
-     var self = this;
+// var ViewModel = function() {
+//      var self = this;
+//      console.log("hi");
+//      this.datalist = ko.observableArray(my_data);
 
+// }
 
-
-}
-
-var viewModel = new ViewModel();
-ko.applyBindings(viewModel);
+// var viewModel = new ViewModel();
+// ko.applyBindings(viewModel);
