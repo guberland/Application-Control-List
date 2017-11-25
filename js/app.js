2c0f067123375ebcc9638c85edc8515e051e4695
guberland <chaojiang1189@gmail.com>
2017-11-24 20:57:35 -0800

    working version 1.0

--- js/app.js
+++ js/app.js
@@ -3,45 +3,75 @@
 // var my data;
 
 var getData = function (){
-<<<<<<< HEAD
     var theURL = "http://jobs.fortinet.com/test.json"
     var namelist=[];
-=======
-    var theURL = "https://github.com/guberland/Application-Control-List/blob/master/test.json"
 
->>>>>>> origin/master
      $.get(theURL, function( my_var ) {
     new_var = my_var.substring(0,my_var.length-1);
     var parsed_data = JSON.parse( new_var);
 
     viewModel.datalist(parsed_data);
-
     $.each(parsed_data,function(i,app){
         namelist.push(parsed_data[i][0]);
+    });
 
-    })
-    console.log(namelist);
+    var parsed_JSON = parsed_data.map(function(i)
+    {
+        return {
+            app_name:i[0],
+            app_type:i[1],
+            number_col1:i[2],
+            number_col2:i[3],
+            number_col3:i[4],
+            date:i[5],
+            register_name:i[6],
+            website:i[7],
+            ctrl_reason:i[8],
+            explanation:i[9],
+            policy_number:i[10],
+            app_fullname:i[11],
+            action:i[12],
+            URL:i[13],
+            number_col4:i[14],
+            number_col5:i[15],
+            unknown_col:i[16]
+
+    };
+    });
+
+
 
     viewModel.names(namelist);
-}, 'text');     //data from to another place in the load
+    viewModel.datalist(parsed_JSON);
+}, 'text');
 };
 
 window.onload = getData;
 
 
+     $("ul").on("click","button",function(){
+      var name = this.innerHTML;
+      console.log(viewModel.datalist());
+      for (var x of viewModel.datalist()){
+        if ( name == x.app_name)
+        $("#content").html(populateContent(x));}
+
+    });
+
+
+    var populateContent = function(i){
+        return '<h3>'+i.app_name+'</h3>';
+     }
 
 
 
 
 
-<<<<<<< HEAD
 var ViewModel = function() {
     var self = this;
 
-    this.datalist = ko.observableArray();
+    self.datalist = ko.observableArray();
     self.names = ko.observableArray([]);
-    this.query = ko.observable('');
-
     self.query = ko.observable('');
 
     self.filteredNames = ko.pureComputed(function(){
@@ -49,15 +79,8 @@ var ViewModel = function() {
             return item.toLowerCase().indexOf(self.query().toLowerCase()) >= 0;
         });
     });
-
-
-
-
 }
+
 
 var viewModel = new ViewModel();
 ko.applyBindings(viewModel);
-=======
-// var viewModel = new ViewModel();
-// ko.applyBindings(viewModel);
->>>>>>> origin/master
